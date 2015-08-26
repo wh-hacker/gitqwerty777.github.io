fcf499a57818560543ecc426f20d0a1a9c709c7c
HCL <goohcl777@gmail.com>
2015-08-26 15:52:48 +0800

    Site updated: 2015-08-26 15:52:12

--- getSimilarDataFromPTT.js
+++
@@ -1,7 +0,0 @@
-//github page does not support php
-
-$.ajax({
-    url: "./googleExcelRetrieveTest.py",
-    success: function(response){alert("call python script success");
-    }
-});
--- getSimilarDataFromPTT.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$command = escapeshellcmd('./python ./googleExcelRetrieveTest.py');
-$output = shell_exec($command);
-echo $output;
-
-?>
-
